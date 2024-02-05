1a23587c12d5c03c043bcf47a9106323c7d01c91
Sachin Kant <sachinkant404@gmail.com>
2024-02-06 01:31:59 +0530

    fiber app setup

--- api/main.go
+++ api/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello")
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	app := fiber.New()
+
+	app.Use(logger.New())
+
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
 }
